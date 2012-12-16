965d27374fafaedfe0425165e59c99d08d94be6e
David Whitlock <notmyrealaddress@email.com>
2012-12-16 08:04:25 -0800

    Added the skeleton of fixtures for configuring the JMX Data Mart's collector and extractor

---
+++ fixtures/src/test/java/org/jmxdatamart/fixtures/CollectorTestHelper.java
@@ -0,0 +1,11 @@
+package org.jmxdatamart.fixtures;
+
+import fitlibrary.DoFixture;
+
+public class CollectorTestHelper extends DoFixture {
+
+  public void collectStatisticsForSeconds(int collectTime) {
+
+  }
+
+}
---
+++ fixtures/src/test/java/org/jmxdatamart/fixtures/ConfigureMBeansToBeCollected.java
@@ -0,0 +1,9 @@
+package org.jmxdatamart.fixtures;
+
+public class ConfigureMBeansToBeCollected extends  GuicySetUpFixture {
+
+  public void beanNameBeanAliasAttributeAttributeAlias(String beanName, String beanAlias, String attribute, String attributeAlias) {
+
+  }
+
+}
