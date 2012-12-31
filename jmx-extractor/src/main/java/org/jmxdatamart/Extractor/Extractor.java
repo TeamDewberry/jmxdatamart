246bf253f368c04e5e7c6f370f11b6210ada97bc
David Whitlock <notmyrealaddress@email.com>
2012-12-30 20:38:33 -0800

    Demonstrated that when the Extractor is run in its own web application, it can extract MBeans from another web application.  This required that the Extractor run periodically.  I also introduced logback and SLF4J for logging.

--- jmx-extractor/src/main/java/org/jmxdatamart/Extractor/Extractor.java
+++ jmx-extractor/src/main/java/org/jmxdatamart/Extractor/Extractor.java
@@ -29,28 +29,61 @@
 package org.jmxdatamart.Extractor;
 
 import com.google.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.management.*;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Extractor {
 
   private final Settings configData;
   private final MBeanServerConnection mbsc;
+  private final Logger logger = LoggerFactory.getLogger(Extractor.class);
 
   @Inject
   public Extractor(Settings configData, MBeanServerConnection mbsc) {
     this.configData = configData;
     this.mbsc = mbsc;
+
+    if (shouldPeriodicallyExtract()) {
+      periodicallyExtract();
+    }
+  }
+
+  private void periodicallyExtract() {
+    boolean isDaemon = true;
+    Timer timer = new Timer("JMX Statistics Extractor", isDaemon);
+    long rate = configData.getPollingRate();
+    int delay = 0;
+    timer.scheduleAtFixedRate(new Extract(), delay, rate);
+  }
+
+  private boolean shouldPeriodicallyExtract() {
+    return this.configData.getPollingRate() > 0;
   }
 
   void extract() throws MalformedObjectNameException, InstanceNotFoundException, IOException, ReflectionException, AttributeNotFoundException, MBeanException {
-      for (BeanData bd : this.configData.getBeans()) {
-        ObjectName on = new ObjectName(bd.getName());
-        for (Attribute a : bd.getAttributes()) {
-          System.out.println(bd.getAlias() + ", " + a.getAlias() + ", "
-            + a.getDataType() + ", " + this.mbsc.getAttribute(on, a.getName()).toString());
-        }
+    for (BeanData bd : this.configData.getBeans()) {
+      ObjectName on = new ObjectName(bd.getName());
+      for (Attribute a : bd.getAttributes()) {
+        System.out.println(bd.getAlias() + ", " + a.getAlias() + ", "
+          + a.getDataType() + ", " + this.mbsc.getAttribute(on, a.getName()).toString());
       }
+    }
+  }
+
+  private class Extract extends TimerTask {
+    @Override
+    public void run() {
+      try {
+        extract();
+
+      } catch (Exception e) {
+        logger.debug("While extracting MBeans", e);
+      }
+    }
   }
 }
