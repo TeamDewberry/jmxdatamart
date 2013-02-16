eeef56ddbdfac7a42ebb3055606c0ec21ec33c72
David Whitlock <notmyrealaddress@email.com>
2013-02-16 08:45:48 -0800

    Made ExtractAndLoadTest subclass DerbyTest so that the dbfit fixtures are available to end-to-end tests.

--- fitnesse/src/test/java/org/jmxdatamart/fixtures/ExtractAndLoadTest.java
+++ fitnesse/src/test/java/org/jmxdatamart/fixtures/ExtractAndLoadTest.java
@@ -1,12 +1,12 @@
 package org.jmxdatamart.fixtures;
 
-import fitlibrary.DoFixture;
+import dbfit.DerbyTest;
 
 /**
  * This do fixture wraps all of the other fixtures needed to perform an end-to-end
  * extract and load test for the JMX Data Mart.
  */
-public class ExtractAndLoadTest extends DoFixture {
+public class ExtractAndLoadTest extends DerbyTest {
 
   public CreateMBeans createMBeans() {
     return new CreateMBeans();
@@ -24,7 +24,7 @@ public class ExtractAndLoadTest extends DoFixture {
     return new ConfigureMBeansToBeExtracted();
   }
 
-  public void collectStatisticsForSeconds(int collectTime) {
+  public void collectStatisticsFor(int collectTime, String timeUnit) {
 
   }
 
