0009422366ad183b5fc954db5c687271fc930f43
Binh Tran <mynameisbinh@gmail.com>
2013-02-20 23:33:47 -0800

    Modify JMXTestServer.Main to show bug with the implementation of Dynamic MBean

--- jmx-extractor/src/main/java/org/jmxdatamart/JMXTestServer/Main.java
+++ jmx-extractor/src/main/java/org/jmxdatamart/JMXTestServer/Main.java
@@ -27,10 +27,13 @@
  */
 package org.jmxdatamart.JMXTestServer;
 
+import com.sun.jmx.interceptor.DefaultMBeanServerInterceptor;
 import java.lang.management.ManagementFactory;
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
 import java.io.FileWriter;
+import javax.management.Attribute;
+import javax.management.MBeanAttributeInfo;
 import org.jmxdatamart.Extractor.*;
 
 /**
@@ -44,28 +47,12 @@ public class Main {
      */
     public static void main(String[] args) throws Exception {
 
-        TestBean tb = new TestBean();
+        RandomValueDynamicMBean dmb = new RandomValueDynamicMBean();
+        dmb.addAttribute("Car", Car.class.toString(), "a Car", true, true, false);
+        dmb.setAttribute(new Attribute("Car", new Car("123", "234", 3, 2)));
+        
         MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
-        ObjectName mbeanName = new ObjectName("com.personal.JMXTestServer:type=TestBean");
-        mbs.registerMBean(tb, mbeanName);
-
-        FileWriter writer = new FileWriter(System.getProperty("user.dir") + "/JMXTestServer.txt");
-        writer.write("Start exporting on " + new java.util.Date()+"\n");
-        try {
-            while (true) {
-                tb.randomize();
-                System.out.println(tb.toString());
-                writer.write(tb.toString()+"\n");
-                writer.flush();
-                Thread.sleep(2000);
-            }
-        }
-        catch (Exception e){
-            System.err.println(e.getStackTrace());
-            System.exit(1);
-        }
-        finally {
-            writer.close();
-        }
+        ObjectName on = new ObjectName("org.jmxdatamart.jmxtestserver:type=randomDMBean");
+        mbs.registerMBean(dmb, on);
     }
 }
