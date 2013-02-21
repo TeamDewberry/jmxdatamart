7333482f6560d9433e6169b3441c8b3fc520163f
Binh Tran <mynameisbinh@gmail.com>
2013-02-20 22:40:37 -0800

    Added MXNameParser class to parse MXBean attribute names. Incomplete. Addes a set of tests for MXNameParser.

---
+++ jmx-extractor/src/main/java/org/jmxdatamart/Extractor/MXNameParser.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jmxdatamart.Extractor;
+
+/**
+ *
+ * @author Binh Tran <mynameisbinh@gmail.com>
+ */
+public class MXNameParser {
+    
+    public MXNameParser(String s){
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+    
+    public boolean hasNext() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+    
+    public String nextToken() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+    
+}
