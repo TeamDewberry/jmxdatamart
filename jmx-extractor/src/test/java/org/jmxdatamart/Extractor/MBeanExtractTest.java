da3f8ceff3bc44e8ec412479192e2988bb0a408e
Binh Tran <mynameisbinh@gmail.com>
2013-02-26 22:07:14 -0800

    Remove old MXBeanExtractTest because it's not representative. unregister mbean after each test

--- jmx-extractor/src/test/java/org/jmxdatamart/Extractor/MBeanExtractTest.java
+++ jmx-extractor/src/test/java/org/jmxdatamart/Extractor/MBeanExtractTest.java
@@ -75,5 +75,7 @@ public class MBeanExtractTest {
         assertEquals(expected, result.get(a));
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
+        
+        mbs.unregisterMBean(mbeanName);
     }
 }
