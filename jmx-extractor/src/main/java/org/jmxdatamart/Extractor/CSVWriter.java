87457b255a0cd9b1c49005545eadaa40fefcc18d
Binh Tran <mynameisbinh@gmail.com>
2013-02-28 21:46:27 -0800

    Work on CSVWriter

--- jmx-extractor/src/main/java/org/jmxdatamart/Extractor/CSVWriter.java
+++ jmx-extractor/src/main/java/org/jmxdatamart/Extractor/CSVWriter.java
@@ -33,155 +33,157 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import org.jmxdatamart.common.CVSCommon;
+import java.util.TreeMap;
+import org.jmxdatamart.common.CSVCommon;
 import org.jmxdatamart.common.DataType;
 import org.slf4j.LoggerFactory;
 
 /**
  * Class to handle writing to csv file
+ *
  * @author Binh Tran <mynameisbinh@gmail.com>
  */
 public class CSVWriter {
-    
-    //TODO: handle string
-    
-    private final Map<String, DataType> seen;
-    private final String filePath;
-    private final org.slf4j.Logger logger = LoggerFactory.getLogger(CSVWriter.class);
-    
-    
-    /**
-     * Initialize a CSVWriter object by touch a file for BeanData bd,
-     * the  write the BeanData alias on the first line
-     * @param bd related BeanData
-     * @param folderLocation output folder location
-     */
-    public CSVWriter(MBeanData bd, String folderLocation) {
-        seen = new HashMap<String, DataType>();
-        
-        try {
-            BufferedWriter bw;
-            File outputFile = new File(folderLocation, bd.getAlias() + ".csv");
-            if (outputFile.exists()) {
-                logger.info(bd.getAlias() + ".csv already exist, overiding");
-                outputFile.delete();
-            }
-            outputFile.createNewFile();
-            
-            filePath = outputFile.getPath();
-            
-            bw = new BufferedWriter(
-                        new FileWriter(
-                            outputFile,
-                            true
-                        )
-                    );
-            
-            bw.write(bd.getAlias());
-            bw.newLine();
-            bw.close();
-        } catch (IOException ex) {
-            logger.error("Can not open " + bd.getAlias() + ".csv", ex);
-            throw new RuntimeException(ex);
-        }
-    }
-    
-    
-    /**
-     * Enclose a character sequence in double quote
-     * @param cs 
-     * @return 
-     */
-    static StringBuilder enclose(CharSequence cs) {
-        return (new StringBuilder()).
-                append(CVSCommon.GENERAL_ENCLOSE).
-                append(cs).
-                append(CVSCommon.GENERAL_ENCLOSE);
-    }
-    
-    /**
-     * return epoc time as a stringbuilder, enclosed in double
-     * quote & append comma at the end
-     * @return 
-     */
-    static StringBuilder getEpocTime() {
-        Long l = new Long(System.currentTimeMillis());
-        return (new StringBuilder(enclose(l.toString())).append(CVSCommon.DELIMITER));
-    }
-    
-    /**
-     * Line up an alias, its datatype & its value in CSV format, the sequence
-     * DOES NOT end with an delimiter
-     * @param alias alias of the attribute
-     * @param dataType data type of the attribute
-     * @param value actual value of the attribute
-     * @return a character sequence represent all 3 in CSV format.
-     */
-    static StringBuilder lineUp(String alias, String dataType, String value) {
-        StringBuilder sb = new StringBuilder();
-        
-        sb.append(enclose(alias))
-          .append(CVSCommon.DELIMITER);
-        
-        sb.append(enclose(dataType))
-          .append(CVSCommon.DELIMITER);
-        
-        sb.append(enclose(value));
-        
-        return sb;
-    }
-    
-    /**
-     * LineUp all entry in a result set.
-     * @param result the result of an extract
-     * @return a character sequence represents a line
-     */
-    StringBuilder lineUpResult(Map<Attribute, Object> result) {
-        
-        StringBuilder line = new StringBuilder();
-        
-        for (Map.Entry<Attribute, Object> pair : result.entrySet()) {
-            String dataType;
-            if (!seen.containsKey(pair.getKey().getAlias())){
-                seen.put(pair.getKey().getAlias(), pair.getKey().getDataType());
-                dataType = pair.getKey().getDataType().toString();
-            } else {
-                dataType = "";
-            }
-            line.append (
-                    lineUp( pair.getKey().getAlias(),
-                            dataType,
-                            pair.getValue().toString())
-                        )
-                .append(CVSCommon.DELIMITER);
-        }
-        return line.deleteCharAt(line.length() - 1); // remove the last DELIMINATOR
-    }
-    
-    /**
-     * Write results in a result set to disk
-     * @param result result to be written
-     */
-    public void writeResult(Map<Attribute, Object> result) {
-        try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter(filePath, true));
-            
-            bw.write(
-                    getEpocTime().append(lineUpResult(result)).toString()
-                    );
-            bw.newLine();
-            bw.close();
-            
-        } catch (IOException ex) {
-            logger.error("Can not open " + filePath, ex);
-            throw new RuntimeException(ex);
-        }
-    }
 
-    /**
-     * @return the filePath
-     */
-    public String getFilePath() {
-        return filePath;
+  //TODO: handle string
+  private final Map<String, Map<String, DataType>> fileList;
+  private final String filePath;
+  private final org.slf4j.Logger logger = LoggerFactory.getLogger(CSVWriter.class);
+
+  /**
+   * Initialize a CSVWriter
+   *
+   * @param folderLocation output folder location
+   */
+  public CSVWriter(String folderLocation) {
+    fileList = new TreeMap<String, Map<String, DataType>>();
+    this.filePath = folderLocation;
+
+  }
+
+  /**
+   * Enclose a character sequence in double quote
+   *
+   * @param cs
+   * @return
+   */
+  static StringBuilder enclose(CharSequence cs) {
+    return (new StringBuilder()).
+            append(CSVCommon.GENERAL_ENCLOSE).
+            append(cs).
+            append(CSVCommon.GENERAL_ENCLOSE);
+  }
+
+  /**
+   * return epoc time as a stringbuilder, enclosed in double quote & append
+   * comma at the end
+   *
+   * @return
+   */
+  static StringBuilder getEpocTime() {
+    Long l = new Long(System.currentTimeMillis());
+    return (new StringBuilder(enclose(l.toString())).append(CSVCommon.DELIMITER));
+  }
+
+  /**
+   * Line up an alias, its datatype & its value in CSV format, the sequence DOES
+   * NOT end with an delimiter
+   *
+   * @param alias alias of the attribute
+   * @param dataType data type of the attribute
+   * @param value actual value of the attribute
+   * @return a character sequence represent all 3 in CSV format.
+   */
+  static StringBuilder lineUp(String alias, String dataType, String value) {
+    StringBuilder sb = new StringBuilder();
+
+    sb.append(enclose(alias))
+            .append(CSVCommon.DELIMITER);
+
+    sb.append(enclose(dataType))
+            .append(CSVCommon.DELIMITER);
+
+    sb.append(enclose(value));
+
+    return sb;
+  }
+
+  /**
+   * LineUp all entry in a result set.
+   *
+   * @param result the result of an extract
+   * @return a character sequence represents a line
+   */
+  StringBuilder lineUpResult(Map<Attribute, Object> result, Map<String, DataType> seen) {
+
+    StringBuilder line = new StringBuilder();
+
+    for (Map.Entry<Attribute, Object> pair : result.entrySet()) {
+      String dataType;
+      if (!seen.containsKey(pair.getKey().getAlias())) {
+        seen.put(pair.getKey().getAlias(), pair.getKey().getDataType());
+        dataType = pair.getKey().getDataType().toString();
+      } else {
+        dataType = "";
+      }
+      line.append(
+              lineUp(pair.getKey().getAlias(),
+              dataType,
+              pair.getValue().toString()))
+              .append(CSVCommon.DELIMITER);
     }
+    return line.deleteCharAt(line.length() - 1); // remove the last DELIMINATOR
+  }
+
+  /**
+   * Write results in a result set to disk
+   *
+   * @param result result to be written
+   */
+  public void writeResult(String name, Map<Attribute, Object> result) {
+    try {
+      BufferedWriter bw;
+      bw = new BufferedWriter(
+              new FileWriter(
+                filePath + name + ".csv",
+                true));
+      
+      Map<String, DataType> seen;
+      if (fileList.containsKey(name)) {
+        seen = fileList.get(name);
+      } else {
+        seen = new TreeMap<String, DataType>();   // see the file for the first time
+        fileList.put(name, seen);
+        bw.close();
+        
+        File f = new File(filePath + name + ".csv");
+        if (f.exists()) {
+          f.delete();
+        }
+        
+        bw = new BufferedWriter(
+              new FileWriter(
+                filePath + name + ".csv",
+                true));
+        bw.write(name);
+        bw.newLine();
+      }
+
+      bw.write(
+              getEpocTime().append(lineUpResult(result, seen)).toString());
+      bw.newLine();
+      bw.close();
+
+    } catch (IOException ex) {
+      logger.error("Can not open " + filePath, ex);
+      throw new RuntimeException(ex);
+    }
+  }
+
+  /**
+   * @return the filePath
+   */
+  public String getFilePath() {
+    return filePath;
+  }
 }
