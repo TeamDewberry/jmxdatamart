fdc96b7cb8dfbf2c657c5203685fa80d9cf019e4
Afroman808 <Afroman808@131.252.167.128>
2013-03-01 14:09:16 -0800

    Merged Aya's and Binh's Extractor\Settings code. Kept the @override in MBeanData.java, put BeanData.java back into folder, and left Binh's Attribute.java the same.

--- jmx-extractor/src/main/java/org/jmxdatamart/Extractor/MBeanData.java
+++ jmx-extractor/src/main/java/org/jmxdatamart/Extractor/MBeanData.java
@@ -28,8 +28,11 @@
 package org.jmxdatamart.Extractor;
 
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
+import org.slf4j.LoggerFactory;
 
 /**
  * This class contains data related to any MBean such as name, alias and its
@@ -41,10 +44,12 @@ public class MBeanData {
     private String alias;
     private List<Attribute> attributes;
     private boolean enable;
+    private final org.slf4j.Logger logger = LoggerFactory.getLogger(this.getClass());
 
     /**
      * @return the name
      */
+	@Override
     public String getName() {
         return name;
     }
@@ -59,6 +64,7 @@ public class MBeanData {
     /**
      * @return the alias
      */
+	@Override
     public String getAlias() {
         return alias;
     }
@@ -107,6 +113,7 @@ public class MBeanData {
     /**
      * @return the enable
      */
+	@Override
     public boolean isEnable() {
         return enable;
     }
@@ -139,8 +146,13 @@ public class MBeanData {
      * @throws MalformedObjectNameException if MBeanData's name is not a valid
      * ObjectName's name
      */
-    public boolean isPattern() throws MalformedObjectNameException {
+    public boolean isPattern() {
+      try {
         return (new ObjectName(this.name)).isPattern();
+      } catch (MalformedObjectNameException ex) {
+        logger.error("MBean name malformed in " + this.name, ex);
+        return false;
+      }
     }
 
     @Override
